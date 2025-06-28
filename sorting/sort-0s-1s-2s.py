3e6b872ecb35e768ffe17f64fa8c4d9bc0ccfc94
Gyanaranjan Das <infernalwhisper666@gmail.com>
2025-06-28 23:01:35 +0530

    Create sort-0s-1s-2s.py

---
+++ sorting/sort-0s-1s-2s.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr: List[int])->None:
+        # code here
+        low = 0
+        mid = 0
+        high = len(arr)-1
+        
+        while mid<=high:
+            if arr[mid] == 0:
+                arr[low],arr[mid]=arr[mid],arr[low]
+                low+=1
+                mid+=1
+            elif arr[mid] == 1:
+                mid += 1
+            
+            else:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
